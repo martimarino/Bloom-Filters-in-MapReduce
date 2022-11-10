c85f184cd19bb9e7804f2e391fc589963151b35f
lcaprioli96 <93045436+lcaprioli17@users.noreply.github.com>
2022-11-10 13:15:45 +0100

    Spark updates

--- spark/main.py
+++ spark/main.py
@@ -1,10 +1,11 @@
 import sys
 import math
-import pickle
 import mmh3
+import numpy as np
 from bitarray import bitarray
 
 import findspark
+
 findspark.init()
 
 from pyspark import SparkConf, SparkContext
@@ -19,97 +20,112 @@ def compute_k(p):
     return int(-math.log(p) / math.log(2))
 
 
-def positions(rows):
-    global broadcast_k, broadcast_m
-    vote = rows[1]
-    pos = []
-    for i in range(broadcast_k.value):
-        hash_i = mmh3.hash(rows[0], i) % broadcast_m.value[int(round(float(rows[1]))) - 1]
-        pos.append(hash_i)
-    return vote, pos
-
-
-def create_bf(rows):
-    global broadcast_m
-    vote = rows[0]
-    hash_list = rows[1]
-    bf = bitarray(broadcast_m.value[vote - 1])
-    bf.setall(0)
-    for v in hash_list:
-        bf[v] = True
-    return bf
-
-
-def check_bf(rows):
+def create_bf(row):
     global broadcast_m, broadcast_k
-    vote = rows[1]
+    key = row[0]
+    titles = row[1]
+    bf = bitarray(broadcast_m.value[key - 1])
+    bf.setall(0)
+    for title in titles:
+        for i in range(broadcast_k.value):
+            hash_i = abs(mmh3.hash(title, i)) % broadcast_m.value[key - 1]
+            bf[hash_i] = True
+    return key, bf
+
+
+def check_bf(row):
+    global broadcast_m, broadcast_k, rdd_bf
+    key = row[0]
+    title = row[1]
     counter = [0 for _ in range(10)]
     for i in range(10):
         pos_count = 0
-        if i == vote - 1:
+        if i == key - 1:
             continue
         for j in range(broadcast_k.value):
-            hash_j = mmh3.hash(rows[0], j) % broadcast_m.value[int(round(float(vote))) - 1]
-            if rdd_bf.value[i + 1][hash_j]:
+            hash_j = mmh3.hash(title, j) % broadcast_m.value[i]
+            if rdd_bf.value[i][hash_j]:
                 pos_count += 1
         if pos_count == broadcast_k.value:
             counter[i] = 1
-    return counter
+    return key, np.array(counter, dtype=object)
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
-        print("Usage: main.py <dataset> <0/1> <fp>")
-        exit()
+    # if len(sys.argv) != 4:
+    #     print("Usage: main.py <dataset> <0/1> <fp>")
+    #     exit()
 
-    path = sys.argv[1]
-    output = sys.argv[2]
-    p = float(sys.argv[3])
+    # path = sys.argv[1]
+    # output = sys.argv[2]
+    # p = float(sys.argv[3])
 
-    # application name
-    name = "SparkBloomFilters"
+    conf = (SparkConf()
+            .setMaster("local[*]")
+            .setAppName("BFSpark"))
 
     # initialize a new Spark Context to use for the execution of the script
-    sc = SparkContext(appName=name, master="yarn")
+    sc = SparkContext(conf=conf)
+
+    p = 0.01
+    # path = "hdfs://172.16.4.137:8020/dataset.tsv"
+    path = "data.tsv"
+
+    spark = SparkSession(sc)
 
     # STAGE 1
     # loading the csv removing numVotes
-    spark = SparkSession(sc)
+    print("Starting STAGE 1...")
     rdd = spark.read.csv(path, sep='\t', header=True).drop('numVotes').select("tconst", "averageRating").rdd
+    new_dataset = rdd.map(lambda x: (int(round(float(x[1]))), x[0]))
 
+    print("Counting the films per vote...")
     # counting the number of movies per vote
     vote_count = rdd.map(lambda x: (int(round(float(x[1]))), 1)).reduceByKey(lambda x, y: x + y).sortByKey()
 
+    print("Computing M values...")
     # computing the M value for each vote
     m_values = vote_count.map(lambda x: (x[0], compute_m(x[1], p))).sortByKey().values().collect()
 
+    print("Computing movie counts per vote...")
     # collects an ordered array of movie counts per vote
     tot_values = vote_count.values().collect()
 
+    print("Broadcasting variables...")
     # compute k
     k = compute_k(p)
 
     # broadcasts the vector of M values and K to all workers
     broadcast_m = sc.broadcast(m_values)
+    print(m_values)
     broadcast_k = sc.broadcast(k)
+    print(k)
     broadcast_tot = sc.broadcast(tot_values)
+    print(tot_values)
 
     # STAGE 2
-    # find the hashes (positions) for each movie and aggregate them
-    key_hashes = rdd.map(positions).reduceByKey(lambda x, y: x + y)
+    print("Starting STAGE 2...")
+    print("Grouping titles...")
+    title_lists = new_dataset.groupByKey()
 
-    # create the bloom filters
-    bloom_filters = key_hashes.map(create_bf).sortByKey().collect()
+    print("Creating bloom filters...")
+    bloom_filters = title_lists.map(create_bf).reduceByKey(lambda x, y: x & y).sortByKey().values().collect()
+
+    for i in bloom_filters:
+        print(len(i))
 
     # remove the global variables
     # broadcast_m.destroy()
     # broadcast_k.destroy()
 
     # instantiate globally the bloom filters
+    print("Broadcasting bloom filters...")
     rdd_bf = sc.broadcast(bloom_filters)
 
     # STAGE 3
-    fp_set = rdd.map(check_bf).reduce(lambda x, y: x + y).sortByKey()
+    print("Checking False Positives...")
+    fp_set = new_dataset.map(check_bf).reduce(lambda x, y: np.add(np.array(x, dtype=object), np.array(y, dtype=object)))
 
-    print(fp_set)
+    for i in fp_set:
+        print(i)
